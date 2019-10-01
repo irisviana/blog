8deb1860d2987cdce5fc6e0d2453290be76d039d
Iris <vianasantana21@gmail.com>
2019-10-01 17:21:27 -0300

    Teste novo artigo com titulo vazio

--- blog/features/step_definitions/article_steps.rb
+++ blog/features/step_definitions/article_steps.rb
@@ -12,3 +12,17 @@ end
 Then("Eu vejo que o artigo de titulo {string} foi salvo") do |titulo|
   expect(page).to have_content(titulo)
 end
+
+When("Eu preencho o artigo com titulo {string} e texto {string}") do |titulo, corpo|
+  fill_in 'article[title]', :with =>titulo
+  fill_in 'article[text]', :with =>corpo
+end
+
+When("Eu clico em salvar artigo") do
+  click_button 'submit'
+end
+
+Then("Eu vejo uma mensagem de artigo invalido") do
+  assert_selector('div#error_explanation', text: '')
+end
+
--- blog/features/support/article.feature
+++ blog/features/support/article.feature
@@ -7,3 +7,9 @@ Feature: Artigo
     Given Eu abro a pagina de criar um novo artigo
     When Eu crio o artigo com titulo 'novo artigo Ruby' e texto 'Ruby'
     Then Eu vejo que o artigo de titulo 'novo artigo Ruby' foi salvo
+
+  Scenario: novo artigo com titulo vazio
+    Given Eu abro a pagina de criar um novo artigo
+    When Eu preencho o artigo com titulo '' e texto ''
+    And Eu clico em salvar artigo
+    Then Eu vejo uma mensagem de artigo invalido
